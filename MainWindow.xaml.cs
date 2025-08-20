3ff00c0499464393b613de92bf68367456031327
Mark Anthony Asuncion <maasuncion@sscgi.com>
2025-08-20 13:34:32 +0800

    Initial Currency Converter

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFCurrencyConverter.Utils;
+
+namespace WPFCurrencyConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        struct WindowDataContext
+        {
+            public string[] Currencies { get; set; }
+            public int IdFromCurrency { get; set; }
+            public int IdToCurrency { get; set; }
+            public double AmountToConvert { get; set; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new WindowDataContext()
+            {
+                Currencies = ServiceConverter.CURRENCIES,
+                IdFromCurrency = 0,
+                IdToCurrency = 1,
+                AmountToConvert = 0.0
+            };
+        }
+
+        private async void OnConvert(object sender, RoutedEventArgs e)
+        {
+            WindowDataContext ctx = (WindowDataContext)DataContext;
+            MessageBox.Show($"{ctx.AmountToConvert} {ctx.Currencies[ctx.IdFromCurrency]} -> {ctx.Currencies[ctx.IdToCurrency]}");
+
+            var converter = new ServiceConverter(ctx.AmountToConvert, ctx.Currencies[ctx.IdFromCurrency], ctx.Currencies[ctx.IdToCurrency]);
+            BtnConvert.IsEnabled = false;
+            BtnClear.IsEnabled = false;
+            try
+            {
+                double converted = await converter.Convert();
+                InpToAmount.Text = $"{converted}";
+                LbError.Content = "";
+            }
+            catch (Exception ex)
+            {
+                LbError.Content = ex.Message;
+                InpToAmount.Text = "";
+            }
+            finally
+            {
+                BtnConvert.IsEnabled = true;
+                BtnClear.IsEnabled = true;
+            }
+        }
+
+        private void OnClear(object sender, RoutedEventArgs e)
+        {
+            WindowDataContext ctx = (WindowDataContext)DataContext;
+            InpFromAmount.Text = "0";
+            InpToAmount.Text = "";
+            ctx.AmountToConvert = 0;
+        }
+    }
+}
---
+++ Utils/ServiceConverter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WPFCurrencyConverter.Utils
+{
+    class ApiResult
+    {
+        public Dictionary<string, double> rates { get; set; }
+    }
+
+    public class ServiceConverter
+    {
+
+        public static readonly string[] CURRENCIES = {
+            "USD",
+            "EUR",
+            "JPY",
+            "GBP",
+            "AUD",
+            "CAD",
+            "CHF",
+            "CNY",
+            "HKD",
+            "NZD",
+            "SEK",
+            "KRW",
+            "SGD",
+            "NOK",
+            "MXN",
+            "INR",
+            "RUB",
+            "ZAR",
+            "BRL"
+        };
+        private double FromAmount;
+        private string FromType;
+        private string ToType;
+
+        public ServiceConverter(double amount, string fromType, string toType)
+        {
+            FromAmount = amount;
+            FromType = fromType;
+            ToType = toType;
+        }
+
+        public async Task<double> Convert()
+        {
+            if (FromType == ToType) return FromAmount;
+            HttpClient client = new HttpClient();
+            Uri uri = new Uri($"https://open.er-api.com/v6/latest/{FromType}");
+            try
+            {
+                ApiResult? res = await client.GetFromJsonAsync<ApiResult>(uri);
+                if (res == null) throw new Exception("Res is null");
+                double rate = res.rates[ToType];
+                return FromAmount* rate;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"{e.GetType()}::{e.Message}");
+                throw new Exception("Failed to Convert Amount");
+            }
+        }
+    }
+}
